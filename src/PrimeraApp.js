4c9470f24c84a45ad0fd6af5f1ab8e4b5df7114f
ManuelVanegas <mvanegasp@ucentral.edu.co>
2022-01-08 17:08:24 -0500

    first commit

---
+++ src/PrimeraApp.js
@@ -0,0 +1,24 @@
+// Functional Component
+
+import React from "react";
+import PropTypes from "prop-types";
+
+const  PrimeraApp = ( {saludo, subtitulo} ) => {
+
+    return (
+        <>
+            <h1> {saludo} </h1>
+            <p> {subtitulo} </p>
+        </>
+    );
+}
+
+PrimeraApp.propTypes = {
+    saludo: PropTypes.string.isRequired
+}
+
+PrimeraApp.defaultProps = {
+    subtitulo: 'Soy un subtítulo'
+}
+
+export default PrimeraApp;
---
+++ src/test/base/02-template-string.test.js
@@ -0,0 +1,22 @@
+import '@testing-library/jest-dom'
+import { getSaludo } from "../../base/02-template-string";
+
+describe('Pruebas en 02-template-string.js', () => {
+
+    test('getSaludo debe retornar Hola Manuel ', () => {
+
+        const nombre = 'Manuel';
+        
+        const saludo = getSaludo(nombre);
+
+        expect(saludo).toBe('Hola ' + nombre+'!');
+    })
+     
+    test('getSaludo debe de retornar Hola Carlos! si no hay argumento en el nombre ', () => {
+
+        const saludo = getSaludo();
+
+        expect(saludo).toBe('Hola Carlos!');
+    })
+
+})
